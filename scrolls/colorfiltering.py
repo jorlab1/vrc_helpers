342900791b94d555eb6405b403f51756e3b7fe33
VRC <jlabuda@uchicago.edu>
2021-12-01 15:47:43 -0600

    starting to work on vertice stuff

--- scrolls/colorfiltering.py
+++ scrolls/colorfiltering.py
@@ -2,6 +2,7 @@
 import numpy as np
 from PIL import Image
 import cv2
+import imutils
 
 def edit(fname,up):
     image = cv2.imread(fname)
@@ -29,10 +30,48 @@ def edit(fname,up):
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    return None
+    return mask
 
+def edge(image):
+    
+    edgeMap = imutils.auto_canny(image)
+    cv2.imshow("Original", image)
+    cv2.imshow("Automatic Edge Map", edgeMap)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return edgeMap
 
 if __name__=='__main__':
     #edit('for testing.tif')
-    edit('scroll1.tif',215)
+    im = cv2.imread('for testing.tif')
+    masked = edit('for testing.tif',180)
+    edges = edge(masked)
+    
+    print(edges.dtype)
+    #unique, counts = np.unique(edges, return_counts=True)
+    #print(np.asarray((unique,counts)).T)
 
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    #cv2.imshow(contours )
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    
+    print("Number of Contours found = " + str(len(contours)))
+    fun=contours
+    for c in fun:
+        vert=len(c)
+        print('Number of vertices = '+str(vert))
+        
+            
+    
+    
+    
+    img_contours = np.zeros(masked.shape)
+    # draw the contours on the empty image
+    cv2.drawContours(img_contours, contours, -1, (255,255,255), 1)
+    cv2.imshow('contours',img_contours )
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    print(len(contours))
+ 
